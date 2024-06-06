448495084f7fa3bf32e33b88756a1134e97da5c3
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-06-06 20:06:37 +0600

    added support for dev mode

--- src/store/services/apiSlice.ts
+++ src/store/services/apiSlice.ts
@@ -1,7 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const baseUrl = process.env.NEXT_PUBLIC_DEV_MODE
+  ? "http://localhost:3000/api"
+  : process.env.NEXT_PUBLIC_FOOD_API;
+
 const baseQuery = fetchBaseQuery({
-  baseUrl: `${process.env.NEXT_PUBLIC_FOOD_API}`,
+  baseUrl: baseUrl,
   credentials: "include",
 });
 
