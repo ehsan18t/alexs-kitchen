3a1c81a2f2ac0aef3526947457918dc343cc7127
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2024-06-06 20:06:14 +0600

    fixed add button size flickering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,15 +10,17 @@ export default function Home() {
   return (
     <main className="py-6 px-4 flex flex-col gap-5">
       <div className="flex justify-center items-center">
-        <Button
-          className="w-4/5 md:w-2/3 lg:w-1/3 ring-1 ring-primary hover:bg-primary hover:text-white"
-          color="primary"
-          size="large"
-          fullWidth
-          variant="outlined"
-        >
-          Add Item
-        </Button>
+        <div className="w-4/5 md:w-2/3 lg:w-1/3">
+          <Button
+            className=" ring-1 ring-primary hover:bg-primary hover:text-white"
+            color="primary"
+            size="large"
+            fullWidth
+            variant="outlined"
+          >
+            Add Item
+          </Button>
+        </div>
       </div>
       <FoodGrid />
     </main>
